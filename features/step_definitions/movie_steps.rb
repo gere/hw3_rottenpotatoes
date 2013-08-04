fd03e99f3591a0e832101e71acb05bc2ae2c288b
gere <gerevini@gmail.com>
2013-08-04 15:29:26 -0700

    part2 almost completed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,5 +1,4 @@
 # Add a declarative step here for populating the DB with movies.
-
 #movies_table = Movie.all
 
 Given /the following movies exist/ do |movies_table|
@@ -8,7 +7,7 @@ Given /the following movies exist/ do |movies_table|
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
   end
-  debugger
+  # debugger
   # flunk "Unimplemented"
 end
 
@@ -26,7 +25,30 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(", ").each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings[#{rating}]"}
+    else
+      step %Q{I check "ratings[#{rating}]"}
+    end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  end
 end
+
+Then /I should (not )?see the following movies:/ do | n, movies_table |
+  if n
+    movies_table.hashes.each do |movie|
+      step %Q{I should not see "#{movie[:title]}"}
+      #debugger
+
+    end
+  else
+    movies_table.hashes.each do |movie|
+      #debugger
+      step %Q{I should see "#{movie[:title]}"}
+    end
+  end
+end
+
