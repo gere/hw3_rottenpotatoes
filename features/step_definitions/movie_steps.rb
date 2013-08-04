7ed5055ec1205b6f957b6bc2deb5342d9da44a92
gere <gerevini@gmail.com>
2013-08-04 07:31:26 -0700

    part1 done

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,15 @@
 # Add a declarative step here for populating the DB with movies.
 
+#movies_table = Movie.all
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  debugger
+  # flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
